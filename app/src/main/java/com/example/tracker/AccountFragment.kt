e54ecce73ace72db931bd89f3c4cbeebdee5beaa
Kurt Sanor <sanorkurtkelly@gmail.com>
2025-10-18 23:47:34 +0800

    feat: implement logout

--- app/src/main/java/com/example/tracker/AccountFragment.kt
+++ app/src/main/java/com/example/tracker/AccountFragment.kt
@@ -1,10 +1,12 @@
 package com.example.tracker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class AccountFragment : Fragment() {
     override fun onCreateView(
@@ -15,5 +17,15 @@ class AccountFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val buttonLogout = view.findViewById<TextView>(R.id.logout)
+
+        buttonLogout.setOnClickListener {
+            val loginPage = Intent(requireContext(), MainActivity:: class.java)
+            startActivity(loginPage)
+        }
+    }
 
 }
